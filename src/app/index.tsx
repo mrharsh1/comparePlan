74490287c4c97b104b9cd6799e0c3066379724c4
Harshit Maurya <130442677+mrharsh1@users.noreply.github.com>
2024-11-19 14:03:49 +0530

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -19,7 +19,11 @@ export default function Home() {
   const [selectedPlans, setSelectedPlans] = useState<
     { planName: string; insurerName: string }[]
   >([]);
-const baseUrl= "https://compareplan-1.onrender.com";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://compareplan-1.onrender.com"
+    : "http://localhost:10000";
+
   // Fetch insurers from the API on component mount
   useEffect(() => {
     const fetchInsurers = async () => {
